3db432c6c74ac3e4095e761a2bf8cfb60209326e
Gaurav Sarawgi <gaurav.sarawgi@outlook.com>
2015-03-27 17:52:38 +0530

    Get Geo Project

---
+++ GeoLib.Services/GeoManager.cs
@@ -0,0 +1,47 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Services
+{
+    public class GeoManager:IGeoService     
+    {
+
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            ZipCodeData zipCodeData = null;
+            IZipCodeRepository zipCodeRepository = new ZipCodeRepository();
+            ZipCode zipCodeEntity = zipCodeRepository.GetByZip(zip);
+            if (zipCodeData!=null)
+            {
+                zipCodeData = new ZipCodeData
+                {
+                    City = zipCodeEntity.City,
+                    State = zipCodeEntity.State.Abbreviation,
+                    ZipCode = zipCodeEntity.Zip
+                };
+            }
+
+            return zipCodeData;
+        }
+
+        public IEnumerable<string> GetStates(bool primaryStates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
