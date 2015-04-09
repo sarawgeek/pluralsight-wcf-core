321210700967a974d7d7578a2d7834726e8d6fba
Gaurav <sarawgeek@gmail.com>
2015-04-10 00:49:23 +0530

    Console host Added

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using GeoLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+            hostGeoManager.Open();
+
+            Console.WriteLine("Service Started, Press enter to Close");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
